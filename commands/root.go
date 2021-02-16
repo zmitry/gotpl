79f21bf6e507d630618cc8f4335d1d5dc74756ee
Volodymyr Kuznetsov <vova@cyberhaven.com>
2021-02-16 08:15:36 -0800

    Print errors to stderr

--- commands/root.go
+++ commands/root.go
@@ -45,14 +45,14 @@ func validateArgs(cmd *cobra.Command, args []string) error {
 
 func runCommand(cmd *cobra.Command, args []string) {
 	if err := tpl.ParseTemplate(args, opt); err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
